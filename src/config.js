4f019b0e93af71a8bbc47e0d7f90af921befd872
Daniel Botha <hashlips@code.local>
2021-09-15 08:55:56 +0200

    Updated: win delimiter fix and layer configuration

--- src/config.js
+++ src/config.js
@@ -2,15 +2,22 @@ const description =
   "This is the description of your NFT project, remember to replace this";
 const baseUri = "https://hashlips/nft";
 
-const layersOrder = [
-  { name: "Background" },
-  { name: "Eyeball" },
-  { name: "Eye color" },
-  { name: "Iris" },
-  { name: "Shine" },
-  { name: "Bottom lid" },
-  { name: "Top lid" },
+const layerConfigurations = [
+  {
+    layerEditionSize: 10,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
 ];
+
+const rarityDelimiter = "*";
 
 const format = {
   width: 512,
@@ -24,14 +31,12 @@ const background = {
 
 const uniqueDnaTorrance = 10000;
 
-const editionSize = 3;
-
 module.exports = {
-  layersOrder,
   format,
-  editionSize,
   baseUri,
   description,
   background,
   uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
 };
