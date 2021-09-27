b672224fe8b1f56d2f037fdf7281a6f7c909b281
Jie <63262825+JiesGit@users.noreply.github.com>
2021-09-28 00:33:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ console.log(path.join(basePath, "/src/config.js"));
 const {
   format,
   baseUri,
-  addStats, // Eyy importing our addStats
   description,
   background,
   uniqueDnaTorrance,
@@ -30,6 +29,10 @@ const ctx = canvas.getContext("2d");
 var metadataList = [];
 var attributesList = [];
 var dnaList = [];
+// Input the range of your random number to be the value of the trait
+let range = 101;
+// This is just the date for the birthday YAY!! 
+let dateTime = Date.now();
 
 const buildSetup = () => {
   if (fs.existsSync(buildDir)) {
@@ -250,9 +253,41 @@ const startCreating = async () => {
     while (
       editionCount <= layerConfigurations[layerConfigIndex].growEditionSizeTo
     ) {
-      addStats.forEach((stats) => {
+      // This is where the awesomeness begins bwahahhahaha
+const addStats = [
+{
+      "trait_type": "Level", // This is the Level of your nft
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+      "trait_type": "Stamina", // You can add stats like Stamina, Dexterity, Luck, etc.
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+// OpenSea currently supports three different options, number, boost_percentage (shows percentage), and boost_number (similar to boost_percentage but doesn't show a percent sign). If you pass in a value that's a number and you don't set a display_type, the trait will appear in the Rankings section
+      "display_type": "boost_number", // According to the Metadata standard this is a field indicating how you would like it to be displayed
+      "trait_type": "Damage", // This will show like "Damage +<random number>"
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+      "display_type": "boost_percentage", 
+      "trait_type": "Stamina Increase", 
+      "value": Math.floor(Math.random() * range)
+    }, 
+// OpenSea also supports a date display_type. Traits of this type will appear in the right column near "Rankings" and "Stats." Pass in a unix timestamp as the value.
+        {
+      "display_type": "date", 
+      "trait_type": "birthday", 
+      "value": dateTime
+    },
+// If you don't want to have a trait_type for a particular trait, you can include just a value in the trait and it will be set as a generic property.
+  {
+    "value": "Happy"
+  }
+];
+addStats.forEach((stats) => {
         attributesList.push(stats);
-      }); // I asked help from Orion he said I should put it here
+      });
       let newDna = createDna(layers);
       if (isDnaUnique(dnaList, newDna)) {
         let results = constructLayerToDna(newDna, layers);
