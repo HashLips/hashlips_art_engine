dcd1c8fc3c772ac13cb420ae6e685d992851bd18
Jie <63262825+JiesGit@users.noreply.github.com>
2021-09-28 17:25:02 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,10 @@ const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
   "This is the description of your NFT project, remember to replace this";
 const baseUri = "ipfs://NewUriToReplace";
+// Input the range of your random number to be the value of the trait
+let range = 101;
+// This is just the date for the birthday YAY!! 
+let dateTime = Date.now();
 
 
 const layerConfigurations = [
@@ -24,7 +28,38 @@ const layerConfigurations = [
   },
 ];
 
-
+// This is where the awesomeness begins bwahahhahaha
+const addStats = [
+{
+      "trait_type": "Level", // This is the Level of your nft
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+      "trait_type": "Stamina", // You can add stats like Stamina, Dexterity, Luck, etc.
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+// OpenSea currently supports three different options, number, boost_percentage (shows percentage), and boost_number (similar to boost_percentage but doesn't show a percent sign). If you pass in a value that's a number and you don't set a display_type, the trait will appear in the Rankings section
+      "display_type": "boost_number", // According to the Metadata standard this is a field indicating how you would like it to be displayed
+      "trait_type": "Damage", // This will show like "Damage +<random number>"
+      "value": Math.floor(Math.random() * range) // This generates a random number based on your specific range
+    }, 
+    {
+      "display_type": "boost_percentage", 
+      "trait_type": "Stamina Increase", 
+      "value": Math.floor(Math.random() * range)
+    }, 
+// OpenSea also supports a date display_type. Traits of this type will appear in the right column near "Rankings" and "Stats." Pass in a unix timestamp as the value.
+        {
+      "display_type": "date", 
+      "trait_type": "birthday", 
+      "value": dateTime
+    },
+// If you don't want to have a trait_type for a particular trait, you can include just a value in the trait and it will be set as a generic property.
+  {
+    "value": "Happy"
+  }
+];
 
 
 const shuffleLayerConfigurations = false;
@@ -58,6 +93,7 @@ module.exports = {
   format,
   baseUri,
   description,
+  addStats, // Export the module
   background,
   uniqueDnaTorrance,
   layerConfigurations,
