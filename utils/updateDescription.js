d5ab94994fdc5f1ed48dbeafeb1d5b1706271035
Jie <63262825+JiesGit@users.noreply.github.com>
2021-10-03 15:07:29 +0800

    Create updateDescription.js

---
+++ utils/updateDescription.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const fs = require("fs");
+
+console.log(path.join(basePath, "/src/config.js"));
+const { description } = require(path.join(basePath, "/src/config.js"));
+
+// read json data
+let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+let data = JSON.parse(rawdata);
+
+data.forEach((item) => {
+  item.description = `${description}`;
+  fs.writeFileSync(
+    `${basePath}/build/json/${item.id}.json`,
+    JSON.stringify(item, null, 2)
+  );
+});
+
+fs.writeFileSync(
+  `${basePath}/build/json/_metadata.json`,
+  JSON.stringify(data, null, 2)
+);
+
+console.log(`Updated description for images to ===> ${description}`);
