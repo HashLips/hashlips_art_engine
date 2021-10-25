63a79107be63eda4f73eece1cc885d6ccb3a2651
genki hagata <0n10ndevision@gmail.com>
2021-10-24 21:05:06 -0700

    Fixes Provenance Hashing

--- utils/provenance.js
+++ utils/provenance.js
@@ -1,22 +1,31 @@
 const keccak256 = require("keccak256");
 const fs = require("fs");
 const chalk = require("chalk");
-
 const path = require("path");
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+console.log({ basePath });
 
 const { buildDir } = require(path.join(basePath, "/src/config.js"));
 // Read files from the build folder defined in config.
 const metadata = JSON.parse(
-  fs.readFileSync(path.join(buildDir, `/json/_metadata.json`))
+  fs.readFileSync(path.join(buildDir, `/json/_metadata.json`), "utf-8")
 );
 
 const accumulatedHashString = metadata.reduce((acc, item) => {
   return acc.concat(item.imageHash);
 }, []);
 
-const provenance = keccak256(accumulatedHashString).toString("hex");
+const provenance = keccak256(accumulatedHashString.join("")).toString("hex");
 
-console.log("\nSave your Provenance Hash!\n");
+fs.writeFileSync(
+  `${buildDir}/_provenance.json`,
+  JSON.stringify(
+    { provenance, concatenatedHashString: accumulatedHashString.join("") },
+    null,
+    "\t"
+  )
+);
+
+console.log(`\nProvenance Hash Save in !\n${buildDir}/_provenance.json\n`);
 console.log(chalk.greenBright.bold(`${provenance} \n`));
