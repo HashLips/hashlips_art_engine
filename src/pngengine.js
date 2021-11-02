5e3dc9cb328a266c9136417ef6c298097ea30e86
mornail <er.anilmor@gmail.com>
2021-11-03 00:50:41 +0530

    svgengine and pngengine file added

---
+++ src/pngengine.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+const fs = require("fs");
+
+const ImageEngine = {    
+
+};
+
+
+module.exports = { ImageEngine };
---
+++ src/svgengine.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const fs = require("fs");
+
+const ImageEngine = { 
+
+};
+
+
+module.exports = { ImageEngine };
