153177e46f617e77623ee2f790a0fb4a2bbd2015
Imadmahdi2 <34369778+Imadmahdi2@users.noreply.github.com>
2021-11-05 10:36:45 -0700

    Updated  HashlipsGiffer.js Arrow Function
    Corrected : Arrow Function initialization

--- modules/HashlipsGiffer.js
+++ modules/HashlipsGiffer.js
@@ -12,22 +12,22 @@ class HashLipsGiffer {
     this.initGifEncoder();
   }
 
-  initGifEncoder = () => {
+  initGifEncoder () {
     this.gifEncoder = new GifEncoder(this.canvas.width, this.canvas.height);
     this.gifEncoder.setQuality(this.quality);
     this.gifEncoder.setRepeat(this.repeat);
     this.gifEncoder.setDelay(this.delay);
   };
 
-  start = () => {
+  start ()  {
     this.gifEncoder.start();
   };
 
-  add = () => {
+  add () {
     this.gifEncoder.addFrame(this.ctx);
   };
 
-  stop = () => {
+  stop() {
     this.gifEncoder.finish();
     const buffer = this.gifEncoder.out.getData();
     writeFile(this.fileName, buffer, (error) => {});
