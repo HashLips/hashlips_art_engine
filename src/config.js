923e36926e0a1cf3340f97b09ca6873b0e342693
hashlips <hashlipsnft@gmail.com>
2021-11-19 12:50:21 +0200

    Updated

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 20,
     layersOrder: [
       { name: "Background" },
       { name: "Eyeball" },
