4f564e11384bb485a37ba1cf05a10c07d9f796bf
Jordan Morales <52443263+Jordan-Morales@users.noreply.github.com>
2021-12-28 01:11:25 -0600

    Cleaned
    Took file_url and applied to image parameter.

--- utils/Secondary/jsonCleaner.js
+++ utils/Secondary/jsonCleaner.js
@@ -3,9 +3,14 @@ var fs = require('fs');
 for (let index = 1; index < 1001; index++) {
     const fileName = index;
     let rawdata = fs.readFileSync(`${basePath}/build/json/${fileName}.json`);
-    var json = JSON.parse(rawdata);
-    var imagePath = json.image;
-    console.log(imagePath);
+    var metaData = JSON.parse(rawdata);
+
+    metaData.image = metaData.file_url;
+
+    fs.writeFileSync(
+        `${basePath}/build/json/${fileName}.json`,
+        JSON.stringify(metaData, null, 2))
+        console.log(`${fileName}.json updated!`);
 }
 
 
