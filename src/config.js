46fa7d54f7399efc20be06b28d848102d5d88018
Itamar Held <itaheld1990@gmail.com>
2022-03-12 19:45:59 +0200

    revert edition size

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
       { name: "Eyeball" },
