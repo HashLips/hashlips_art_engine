6e32f450dcbd9f0a091ef03b414644245f965385
int96 <int96@users.noreply.github.com>
2022-05-11 18:00:55 -0600

    ok back up and running tim e to make more content
    content

--- src/config.js
+++ src/config.js
@@ -5,14 +5,15 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "ST_AA_SDS";
+const description = "I lost the fundamental again...";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+//external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "https://www.twitch.tv/deedee_lighthouse",
   creators: [
     {
       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
@@ -21,29 +22,46 @@ const solanaMetadata = {
   ],
 };
 
+
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Gradients" },
+      { name: "Spirit" } ,
+      { name: "Vortex" } ,
+      { name: "Spirit Guide" } ,
+      { name: "Birth" } ,
+    ],
+  },
+  // Next Gen::
+  {
+    growEditionSizeTo: 10,
+    layersOrder: [
+      { name: "Gradients" } , 
+      { name: "Spirit" } , 
+      { name: "Birth" } ,
+    ],
+  },
+  // next Gen::
+  {
+    growEditionSizeTo: 15,
+    layersOrder: [
+      { name: "Vortex" } ,
+      { name: "Gradients" } , 
     ],
   },
 ];
+
 
 const shuffleLayerConfigurations = false;
 
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width:  1920,
+  height: 1080,
   smoothing: false,
 };
 
