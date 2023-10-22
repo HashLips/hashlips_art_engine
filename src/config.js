d950d5dc730191236361eefcc00812462932600b
Ozone_Labs <104269339+Ozone-Labs@users.noreply.github.com>
2023-10-22 15:40:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const basePath = process.cwd();
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 
-const network = NETWORK.sol;
+const network = NETWORK.eth;
 
 // General metadata for Ethereum
 const namePrefix = "Your Collection";
